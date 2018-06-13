3110438623bd4fdf459168a07d32bd5b7a6600df
Josh Taylor <joshuataylor157@gmail.com>
2018-06-13 16:10:13 -0600

    Revert "Copied over react-deathmatch app code over the top of create-react-app."

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,11 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route } from 'react-router';
-import { createStore, applyMiddleware, compose } from 'redux';
-import createHistory from 'history/createBrowserHistory';
-import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
-import { createEpicMiddleware } from 'redux-observable';
-import { reducers } from './app/store/reducers';
-import registerServiceWorker from './registerServiceWorker';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import HeroListContainerComponent from './app/hero/heroListContainer';
-import HeroDetailContainerComponent from './app/hero/heroDetailContainer';
-import rootEpic from './app/store/epics';
+import registerServiceWorker from './registerServiceWorker';
 
-declare var window: any;
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
-
-// Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
-const epicMiddleware = createEpicMiddleware(rootEpic)
-let store = createStore(
-    reducers,
-    composeEnhancers(
-        applyMiddleware(epicMiddleware, middleware),
-    ),
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
 );
-
-let root = (
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <div>
-                <Route exact path="/" component={HeroListContainerComponent} />
-                <Route path="/hero/:id" component={HeroDetailContainerComponent}/>
-            </div>
-        </ConnectedRouter>
-    </Provider>
-);
-
-render(root, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
